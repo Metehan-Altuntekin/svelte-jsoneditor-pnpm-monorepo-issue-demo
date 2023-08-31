06543aa88a3351ae7765ff3c92dec1e1db00fe45
Metehan-Altuntekin <86600423+Metehan-Altuntekin@users.noreply.github.com>
2023-08-31 23:26:53 +0300

    create library component

---
+++ library/src/lib/Component.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { JSONEditor } from "svelte-jsoneditor";
+</script>
+
+<div>
+  <h1>Library Component</h1>
+  <JSONEditor />
+</div>
+
+<style>
+  div {
+    background-color: rgb(196, 255, 147);
+    padding: 2em;
+  }
+</style>
